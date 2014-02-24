12dee69f7efe5b179295630a5b17ac1b71d8ccbc
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-02-24 17:07:53 +0900

    Checking in changes prior to tagging of version 1.01.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 926801f..ce267e8 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl extension Router-Boom
    
     {{$NEXT}}
    
    +1.01 2014-02-24T08:07:41Z
    +
    +    - Added EXPERIMENTAL 'Router::Boom::Method#routes' method.
    +
     1.00 2013-11-27T00:25:59Z
    
         - Bump up to 1.00.

--- lib/Router/Boom.pm
+++ lib/Router/Boom.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Carp ();
 
-our $VERSION = "1.00";
+our $VERSION = "1.01";
 
 # Matcher stuff
 our $LEAF_IDX;
--- lib/Router/Boom/Method.pm
+++ lib/Router/Boom/Method.pm
@@ -6,7 +6,7 @@ use 5.008_005;
 
 use Router::Boom;
 
-our $VERSION = "1.00";
+our $VERSION = "1.01";
 
 sub new {
     my $class = shift;
